9779e74408debe34ea86cf0826b3d6f18b001fe3
Jedeng <906810418@qq.com>
2018-04-26 20:38:25 +0800

    create new project

---
+++ XiMaLaYa/XiMaLaYa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiMaLaYa
+//
+//  Created by QT on 2018/4/26.
+//  Copyright © 2018年 Jedeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
