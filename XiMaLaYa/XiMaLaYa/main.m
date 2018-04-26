01a366b55c39c4d4131751d4db95e2ba43ddc8b3
Jedeng <906810418@qq.com>
2018-04-26 20:22:43 +0800

    new project

---
+++ XiMaLaYa/XiMaLaYa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XiMaLaYa
+//
+//  Created by QT on 2018/4/26.
+//  Copyright © 2018年 Jedeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
